8ae23ae1fdf8f91a98d899a6043ebfdbaab09a48
grubusT <143918850+grubusT@users.noreply.github.com>
2025-03-15 18:14:27 +1100

    Update config.py
    Sentiment analysis functionality

--- config.py
+++ config.py
@@ -9,18 +9,30 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY") 
 
-# News Check Configuration
+# Alerts configuration
 NEWS_CHECK_INTERVAL = 900  # 15 minutes in seconds
-FIRST_CHECK_DELAY = 10  # Seconds to wait before first check
+NEWS_CHECK_INITIAL_DELAY = 10  # seconds
 
 # File paths for persistent storage
 SUBSCRIBERS_FILE = "subscribers.json"
 ARTICLES_HISTORY_FILE = "alert_articles.json"
+USER_PREFERENCES_FILE = "user_preferences.json"
 
 # News fetch configuration
 MAX_ALERTS_PER_CHECK = 3
 MAX_LATEST_ARTICLES = 5
 MAX_TRACKED_ARTICLES = 50
+
+# Sentiment score thresholds
+POSITIVE_THRESHOLD = 0.05
+NEGATIVE_THRESHOLD = -0.05
+
+# Sentiment Emojis
+SENTIMENT_EMOJIS = {
+    "positive": "🟢",  # green circle
+    "negative": "🔴",  # red circle
+    "neutral": "⚪"    # white circle
+}
 
 # Print a timestamp message
 def log_message(message):
